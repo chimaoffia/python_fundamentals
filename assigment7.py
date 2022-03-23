66348a8ce406e2149e6cab755c802619a8255654
chimaoffia <offiahchima@gmail.com>
2022-03-23 14:37:13 +0100

    variables,welcome note and age maths done

--- assigment7.py
+++ assigment7.py
@@ -1,2 +1,22 @@
   # create an authomated embassy chat. be creative and have fun with it
-  
+
+
+print("welcome to canadian embassy")
+
+#visa types
+
+leisure = "vacation visa"
+school = "studies visa"
+biz = "business visa"
+
+current_year = 2022
+first_name = input("please enter your first name \n")
+last_name = input("please enter your last name\n")
+year_of_birth = int(input("please enter year of birth\n"))
+
+my_age = current_year - year_of_birth
+
+print(f"welcome {last_name} {first_name}")
+
+print(f"your are {my_age} years old.")
+
