6806960db43b526fb3d088b4b7eef72162d979f9
chimaoffia <offiahchima@gmail.com>
2022-04-03 23:15:08 +0100

    assignment update

---
+++ assignment11.py
@@ -0,0 +1,56 @@
+
+staffs = [
+    {
+       "first_name": "chima",
+        "last_name": "offia",
+        "profession": "software engineer"
+    },
+    {
+        "first_name" : "christian",
+        "last_name" : "nnamani",
+        "hobbies"  : ["hating chelsea","football", "accounting"]
+    },
+    {
+        "first_name": "tochi",
+        "last_name" : "ani",
+        "hobbies" : ["music","football","crypto"]
+    },
+
+    {
+        "first_name": "vincent",
+        "last_name": "offia",
+        "profession": "business"
+    }
+]
+
+staffs[0]["email"] = "offiahchima@gmail"
+print(staffs[0])
+
+# print(staffs)
+# print(staffs[0]["first_name"])
+
+
+# #adding items to a dictionary
+
+# workers = {
+#     "first_name": "chima",
+#     "last_name": "offia"
+# }
+
+# workers["email"] = "offiahchima@gmail.com"
+# workers["last_name"] = "johnson"
+# workers["hobbies"] = ["gaming", "playing chess"]
+
+# print(workers["hobbies"][1])
+
+# #dictionary method update
+
+# workers.update({"last_name":"mike"})
+# workers2 = workers.copy()
+# print(workers2)
+
+# workers.pop("last_name")
+# workers.clear()
+# print(workers.get("nickname", "sorry not found"))
+
+
--- assingment1.py
+++ assingment1.py
@@ -7,3 +7,4 @@ wedding_location = "lafia"
 print("my name is" + " " + first_name + " " + last_name)
 print("in" + " " + date_of_wedding + " " + "i got married to my love" + " " + wife_name)
 print("in" + " " + wedding_location)
+print(f"my name is {first_name} and my last name is {last_name}, on the {date_of_wedding} i married the love of my life {wife_name} in {wedding_location}.")
